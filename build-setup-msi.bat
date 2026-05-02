be79ce497e949c941640c19947640e763bc87fd1
Math Shamenson <8126035+insane66613@users.noreply.github.com>
2026-05-02 12:36:01 -0500

    chore: add MSI setup build launcher

---
+++ build-setup-msi.bat
@@ -0,0 +1,10 @@
+@echo off
+setlocal
+set SCRIPT_DIR=%~dp0
+cd /d "%SCRIPT_DIR%"
+if not exist "%SCRIPT_DIR%node_modules" (
+  call npm install
+  if errorlevel 1 exit /b %ERRORLEVEL%
+)
+call npm run electron:build:msi
+exit /b %ERRORLEVEL%
